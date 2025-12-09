6dc9aa9ae94f5850bfa89de5a74c24056ba768b5
Luke Pitstick <lukepitstick06@gmail.com>
2025-12-09 14:28:08 -0700

    fixed citation

--- app/src/citation.py
+++ app/src/citation.py
@@ -15,23 +15,12 @@ def get_page(url: str):
     response = requests.get(f"{api_url}/extract-citation-info?url={url}")
     return response.json()["citation_info"]
 
-def split_text(text: str, max_length: int = 10000):
-    """
-    Split text into chunks of a given length. This is used to avoid giving the LLM to many tokens which helps to optimize costs and speed.
-    """
-    return [text[i:i+max_length] for i in range(0, len(text), max_length)]
-
-
-
-
 def extract_citation_info(cite_url, date_accessed: datetime.date, registry: ClientRegistry):
     """
     Extract citation info from a website. Uses
     """
     
     text = get_page(cite_url)
-    text = html2txt(text)
-    #text = split_text(text, 15000)[0]
     
     if text is None:
         raise ValueError(f"Failed to extract content from {cite_url}")
